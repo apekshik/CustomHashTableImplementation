8da00384da7543c4faf77b33eeefe187cb36f6aa
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2021-12-08 22:38:39 +0000

    Initiali Commit with Boiler Plate Code for Hash Table Implementation. Also added a few text files with custom tokens for unit testing.

---
+++ HashTable.java
@@ -0,0 +1,17 @@
+public class HashTable<T> {
+    private NGen<T> table[]; // the hash table 
+    private int collisions[]; // array same length as hash table that stores number of collisions at each index.
+    private String type = "general"; 
+    
+    public HashTable() {
+
+    }
+    
+    public void add(T item) {}
+    public void display() {}
+
+    public static void main(String[] args) {
+
+        return;
+    }
+}
