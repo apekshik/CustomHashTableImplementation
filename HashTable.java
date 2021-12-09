5766c15f8fd4e4891f6d637df4e128f8bc50132a
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2021-12-09 02:09:46 +0000

    Completed first iteration of add() method in HashTable.java

--- HashTable.java
+++ HashTable.java
@@ -48,11 +48,28 @@ public class HashTable<T> {
             case 3:
                 index = h3(item);
                 break;
+            default:
+                index = -1;
+                break;
         }
 
+        if (index == -1) 
+            throw new RuntimeException("switch statement in add() method failed for some reason");
+
         // once we've determined the index, the index can either have 0 elements in the linked list or more than 0.
-        // if 0 (so table[index] == null), then we add the first element to this index. 
-        if (table[index] == null) 
+        // if 0 (so table[index] == null), then we add the first element to this index. Each index houses a dummy head
+        // for a linked list (the chain of hashes mapped to that index)
+        if (table[index].getNext() == null) {
+            table[index].setNext(new NGen<T>(item, null));
+            chainlength[index]++; // increment chainlength because we added a new element.
+        }
+        else { // if there are elements already present in the linked list at the desired index.
+            NGen<T> temp1 = new NGen<T>(item, null);
+            NGen<T> temp2 = table[index].getNext();
+            table[index].setNext(temp1);
+            temp1.setNext(temp2);
+            chainlength[index]++; // increment chainlength because we added a new element.
+        }
     } 
 
     // display the entire hash table with the index and number of collisions at each index.
